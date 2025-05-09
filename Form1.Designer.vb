e1eff57c3514f2bd9d42dbca832a8c5f7f44e763
Mrjelly84 <160085192+Mrjelly84@users.noreply.github.com>
2025-05-09 15:38:09 +0100

    Add files via upload
    almost finished , need workout of kinks

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,35 +23,36 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.cboDuration = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
-        Me.Label9 = New System.Windows.Forms.Label()
-        Me.Label11 = New System.Windows.Forms.Label()
-        Me.Label13 = New System.Windows.Forms.Label()
-        Me.Label12 = New System.Windows.Forms.Label()
-        Me.Label10 = New System.Windows.Forms.Label()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.lblSubtotal = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.lblBalance = New System.Windows.Forms.Label()
+        Me.chkWaiver = New System.Windows.Forms.CheckBox()
+        Me.chkInsurance = New System.Windows.Forms.CheckBox()
+        Me.btnCalulate = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
         Me.errorProvider = New System.Windows.Forms.ErrorProvider(Me.components)
         Me.lstEquipment = New System.Windows.Forms.ListBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.txtDeposit = New System.Windows.Forms.TextBox()
         CType(Me.errorProvider, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'ComboBox1
+        'cboDuration
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"1 day", "2 days", "3 days", "1 week", "2 weeks"})
-        Me.ComboBox1.Location = New System.Drawing.Point(195, 50)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(164, 21)
-        Me.ComboBox1.TabIndex = 0
+        Me.cboDuration.FormattingEnabled = True
+        Me.cboDuration.Items.AddRange(New Object() {"1 day", "2 days", "3 days", "1 week", "2 weeks"})
+        Me.cboDuration.Location = New System.Drawing.Point(195, 50)
+        Me.cboDuration.Name = "cboDuration"
+        Me.cboDuration.Size = New System.Drawing.Size(164, 21)
+        Me.cboDuration.TabIndex = 0
         '
         'Label1
         '
@@ -85,9 +86,9 @@ Partial Class Form1
         Me.Label6.AutoSize = True
         Me.Label6.Location = New System.Drawing.Point(409, 58)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.Size = New System.Drawing.Size(46, 13)
         Me.Label6.TabIndex = 6
-        Me.Label6.Text = "Label6"
+        Me.Label6.Text = "Subtotal"
         '
         'Label7
         '
@@ -107,92 +108,82 @@ Partial Class Form1
         Me.Label8.TabIndex = 8
         Me.Label8.Text = "Bal due"
         '
-        'Label9
+        'lblDate
         '
-        Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(494, 16)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(39, 13)
-        Me.Label9.TabIndex = 9
-        Me.Label9.Text = "Label9"
+        Me.lblDate.AutoSize = True
+        Me.lblDate.Location = New System.Drawing.Point(494, 16)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(39, 13)
+        Me.lblDate.TabIndex = 9
+        Me.lblDate.Text = "Label9"
         '
-        'Label11
+        'lblSubtotal
         '
-        Me.Label11.AutoSize = True
-        Me.Label11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label11.Location = New System.Drawing.Point(488, 58)
-        Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(48, 15)
-        Me.Label11.TabIndex = 12
-        Me.Label11.Text = "      0.00"
+        Me.lblSubtotal.AutoSize = True
+        Me.lblSubtotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSubtotal.Location = New System.Drawing.Point(488, 58)
+        Me.lblSubtotal.Name = "lblSubtotal"
+        Me.lblSubtotal.Size = New System.Drawing.Size(48, 15)
+        Me.lblSubtotal.TabIndex = 12
+        Me.lblSubtotal.Text = "      0.00"
         '
-        'Label13
+        'lblTax
         '
-        Me.Label13.AutoSize = True
-        Me.Label13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.Label13.Location = New System.Drawing.Point(485, 136)
-        Me.Label13.Name = "Label13"
-        Me.Label13.Size = New System.Drawing.Size(51, 15)
-        Me.Label13.TabIndex = 10
-        Me.Label13.Text = "              "
+        Me.lblTax.AutoSize = True
+        Me.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTax.Location = New System.Drawing.Point(485, 92)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(48, 15)
+        Me.lblTax.TabIndex = 14
+        Me.lblTax.Text = "      0.00"
         '
-        'Label12
+        'lblBalance
         '
-        Me.Label12.AutoSize = True
-        Me.Label12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label12.Location = New System.Drawing.Point(485, 92)
-        Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(48, 15)
-        Me.Label12.TabIndex = 14
-        Me.Label12.Text = "      0.00"
+        Me.lblBalance.AutoSize = True
+        Me.lblBalance.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblBalance.Location = New System.Drawing.Point(485, 173)
+        Me.lblBalance.Name = "lblBalance"
+        Me.lblBalance.Size = New System.Drawing.Size(48, 15)
+        Me.lblBalance.TabIndex = 15
+        Me.lblBalance.Text = "      0.00"
         '
-        'Label10
+        'chkWaiver
         '
-        Me.Label10.AutoSize = True
-        Me.Label10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label10.Location = New System.Drawing.Point(485, 173)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(48, 15)
-        Me.Label10.TabIndex = 15
-        Me.Label10.Text = "      0.00"
+        Me.chkWaiver.AutoSize = True
+        Me.chkWaiver.Location = New System.Drawing.Point(200, 132)
+        Me.chkWaiver.Name = "chkWaiver"
+        Me.chkWaiver.Size = New System.Drawing.Size(94, 17)
+        Me.chkWaiver.TabIndex = 16
+        Me.chkWaiver.Text = "Liability waiver"
+        Me.chkWaiver.UseVisualStyleBackColor = True
         '
-        'CheckBox1
+        'chkInsurance
         '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(200, 132)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(94, 17)
-        Me.CheckBox1.TabIndex = 16
-        Me.CheckBox1.Text = "Liability waiver"
-        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.chkInsurance.AutoSize = True
+        Me.chkInsurance.Location = New System.Drawing.Point(200, 94)
+        Me.chkInsurance.Name = "chkInsurance"
+        Me.chkInsurance.Size = New System.Drawing.Size(115, 17)
+        Me.chkInsurance.TabIndex = 17
+        Me.chkInsurance.Text = "Damage insurance"
+        Me.chkInsurance.UseVisualStyleBackColor = True
         '
-        'CheckBox2
+        'btnCalulate
         '
-        Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Location = New System.Drawing.Point(200, 94)
-        Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(115, 17)
-        Me.CheckBox2.TabIndex = 17
-        Me.CheckBox2.Text = "Damage insurance"
-        Me.CheckBox2.UseVisualStyleBackColor = True
+        Me.btnCalulate.Location = New System.Drawing.Point(170, 183)
+        Me.btnCalulate.Name = "btnCalulate"
+        Me.btnCalulate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalulate.TabIndex = 18
+        Me.btnCalulate.Text = "Calculate"
+        Me.btnCalulate.UseVisualStyleBackColor = True
         '
-        'Button1
+        'btnClose
         '
-        Me.Button1.Location = New System.Drawing.Point(170, 183)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 18
-        Me.Button1.Text = "Calculate"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(284, 183)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 19
-        Me.Button2.Text = "Close"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnClose.Location = New System.Drawing.Point(284, 183)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 19
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
         '
         'errorProvider
         '
@@ -207,28 +198,46 @@ Partial Class Form1
         Me.lstEquipment.Size = New System.Drawing.Size(120, 108)
         Me.lstEquipment.TabIndex = 20
         '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(26, 192)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(89, 13)
+        Me.LinkLabel1.TabIndex = 21
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Weather forecast"
+        '
+        'txtDeposit
+        '
+        Me.txtDeposit.Location = New System.Drawing.Point(472, 133)
+        Me.txtDeposit.Name = "txtDeposit"
+        Me.txtDeposit.Size = New System.Drawing.Size(71, 20)
+        Me.txtDeposit.TabIndex = 22
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(555, 230)
+        Me.Controls.Add(Me.txtDeposit)
+        Me.Controls.Add(Me.LinkLabel1)
         Me.Controls.Add(Me.lstEquipment)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.CheckBox2)
-        Me.Controls.Add(Me.CheckBox1)
-        Me.Controls.Add(Me.Label10)
-        Me.Controls.Add(Me.Label12)
-        Me.Controls.Add(Me.Label11)
-        Me.Controls.Add(Me.Label13)
-        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnCalulate)
+        Me.Controls.Add(Me.chkInsurance)
+        Me.Controls.Add(Me.chkWaiver)
+        Me.Controls.Add(Me.lblBalance)
+        Me.Controls.Add(Me.lblTax)
+        Me.Controls.Add(Me.lblSubtotal)
+        Me.Controls.Add(Me.lblDate)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.cboDuration)
         Me.Name = "Form1"
         Me.Text = "Winter Sport Rentals"
         CType(Me.errorProvider, System.ComponentModel.ISupportInitialize).EndInit()
@@ -237,22 +246,23 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents cboDuration As ComboBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
-    Friend WithEvents Label9 As Label
-    Friend WithEvents Label11 As Label
-    Friend WithEvents Label13 As Label
-    Friend WithEvents Label12 As Label
-    Friend WithEvents Label10 As Label
-    Friend WithEvents CheckBox1 As CheckBox
-    Friend WithEvents CheckBox2 As CheckBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents lblDate As Label
+    Friend WithEvents lblSubtotal As Label
+    Friend WithEvents lblTax As Label
+    Friend WithEvents lblBalance As Label
+    Friend WithEvents chkWaiver As CheckBox
+    Friend WithEvents chkInsurance As CheckBox
+    Friend WithEvents btnCalulate As Button
+    Friend WithEvents btnClose As Button
     Friend WithEvents errorProvider As ErrorProvider
     Friend WithEvents lstEquipment As ListBox
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents txtDeposit As TextBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,64 @@
 ﻿Public Class Form1
+    Private ReadOnly Taxrate As Double = 0.07
+    Private rentalPrices As New Prices
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblDate.Text = Date.Today.ToShortDateString
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        frmWeatherForecast.WebBrowser1.Navigate("https://www.google.com/search?q=weather&sca_esv=acc27629dfc230bb&sxsrf=AHTn8zqA1UgZ9aCrVEUak7bqxNbxUvBjjw%3A1746794926958&source=hp&ei=rvkdaPv3N--PhbIPxoW-0AI&iflsig=ACkRmUkAAAAAaB4HvpbxutH_Auu6AgDPbG767k5JbVcD&oq=wea&gs_lp=Egdnd3Mtd2l6IgN3ZWEqAggAMg8QIxiABBgnGIoFGEYYgAIyChAjGIAEGCcYigUyERAuGIAEGJECGMcBGIoFGK8BMggQABiABBiSAzILEAAYgAQYkgMYigUyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATILEAAYgAQYsQMYgwEyCBAAGIAEGLEDMgsQABiABBixAxiDAUjYK1D4CFjQDXABeACQAQCYAVmgAeMBqgEBM7gBAcgBAPgBAZgCBKAC7wGoAgrCAgcQIxgnGOoCwgILEAAYgAQYkQIYigXCAhEQLhiABBixAxjRAxiDARjHAcICDhAAGIAEGLEDGIMBGIoFwgIFEAAYgATCAg4QLhiABBixAxjRAxjHAcICCxAuGIAEGNEDGMcBmAMF8QUezZHde-Nc1ZIHATSgB5smsgcBM7gH6gE&sclient=gws-wiz")
+        frmWeatherForecast.Show()
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+
+    End Sub
+
+    Private Sub btnCalulate_Click(sender As Object, e As EventArgs) Handles btnCalulate.Click
+        If lstEquipment.SelectedIndices.Count = 0 Then
+            errorProvider.SetError(lstEquipment, "Please select an equipment to rent")
+            Return
+        Else
+            errorProvider.SetError(lstEquipment, "")
+        End If
+
+        Dim duration As Prices.DurationType = CType(cboDuration.SelectedIndex, Prices.DurationType)
+        If duration = -1 Then
+            errorProvider.SetError(cboDuration, "Please select valid duration")
+            Return
+        Else
+            errorProvider.SetError(cboDuration, "")
+
+        End If
+        Dim deposit As Double
+        If Not Double.TryParse(txtDeposit.Text, deposit) Then
+            errorProvider.SetError(txtDeposit, "Desposit must be a positve number")
+        Else
+            errorProvider.SetError(txtDeposit, "")
+
+        End If
+
+        Dim subTotal As Double = 0
+        For Each index As Integer In lstEquipment.SelectedIndices
+            Dim equipType As Prices.EquipmentType = CType(index, Prices.EquipmentType)
+            subTotal += rentalPrices.GetRentalPrice(equipType, duration)
+
+        Next
+        Dim insurance As Double = 0
+        If chkInsurance.Checked Then
+            insurance = Prices.InsuranceCost(subTotal)
+
+        End If
+        subTotal += insurance
+        Dim tax As Double = subTotal * Taxrate
+
+        Dim balance As Double = subTotal + tax - deposit
+        lblSubtotal.Text = subTotal.ToString("")
+        lblTax.Text = tax.ToString("n")
+        lblBalance.Text = balance.ToString("c")
+
+    End Sub
 End Class
